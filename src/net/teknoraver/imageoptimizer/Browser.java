d37a31e8fafe82121fa4b4bfb36c69e763c74045
Matteo Croce <rootkit85@yahoo.it>
2013-01-01 17:48:15 +0100

    split strings in different files

--- src/net/teknoraver/imageoptimizer/Browser.java
+++ src/net/teknoraver/imageoptimizer/Browser.java
@@ -21,7 +21,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Rect;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
